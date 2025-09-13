d9494adb0285cfe72b15eb66dce4fc4239fa3e5d
Marcel <kgabinmarcel@gmail.com>
2025-09-13 20:57:16 +0100

    ui

--- app/Http/Controllers/AssistanceController.php
+++ app/Http/Controllers/AssistanceController.php
@@ -216,10 +216,10 @@ foreach ($export->wheelChairTypes as $index => $type) {
        
     }
 
-    //return $data;
+   // return $data;
 
     $lastLine = $data[count($data)-1];
-    $totalAgents = $lastLine[9] ?? 0; // index correspondant à 'Nb d'agents'
+    $totalAgents = $lastLine[5 + count($export->wheelChairTypes)] ?? 0; // index correspondant à 'Nb d'agents'
     
   
 
