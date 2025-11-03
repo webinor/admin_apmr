7e0530e61189d5fcc0c71905e1fefec3added521
Marcel <kgabinmarcel@gmail.com>
2025-11-03 13:57:12 +0100

    ui

--- app/Notifications/Auth/PasswordRequest.php
+++ app/Notifications/Auth/PasswordRequest.php
@@ -9,7 +9,7 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class PasswordRequest extends Notification //implements ShouldQueue
+class PasswordRequest extends Notification  implements ShouldQueue
 {
     use Queueable;
 
