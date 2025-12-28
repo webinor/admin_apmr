c50df2d4c1ecb5a68871cb68053ac420d06dab27
Marcel <kgabinmarcel@gmail.com>
2025-12-28 02:54:22 +0100

    ui

--- resources/views/layouts/partials/_new_modal_invoice.blade.php
+++ resources/views/layouts/partials/_new_modal_invoice.blade.php
@@ -217,9 +217,13 @@
 </button>
 
 
-             {{-- <button type="button" data-action="final" class="invoice-button generateBtn btn btn-success float-end ">
+           @if (!app()->isProduction())
+
+             <button type="button" data-action="final" class="invoice-button generateBtn btn btn-success float-end ">
               <i class="bi bi-file-earmark-pdf me-1"></i> Générer Facture
-            </button> --}}
+            </button>
+               
+           @endif
 
             <button type="button" data-action="preview" class="invoice-button generateBtn btn btn-primary float-end  me-2">
               <i class="bi bi-file-earmark-pdf me-1"></i> Apercu de la Facture
