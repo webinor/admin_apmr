ae35cf37ef6bba076e57344121abb5f621e79317
Marcel <kgabinmarcel@gmail.com>
2026-02-02 20:45:53 +0100

    ui

--- app/Console/Commands/BackfillAssistancesCompany.php
+++ app/Console/Commands/BackfillAssistancesCompany.php
@@ -14,12 +14,24 @@ class BackfillAssistancesCompany extends Command
 
     public function handle()
     {
+        // DB::statement("
+        //     UPDATE assistances a
+        //     JOIN ground_agents ga ON ga.id = a.ground_agent_id
+        //     SET a.company_id = ga.company_id
+        // ");
+
+        // $this->info('assistances.company_id rempli avec succès');
+
+
+    DB::transaction(function () {
         DB::statement("
             UPDATE assistances a
             JOIN ground_agents ga ON ga.id = a.ground_agent_id
             SET a.company_id = ga.company_id
+            WHERE a.company_id IS NULL
         ");
+    });
 
-        $this->info('assistances.company_id rempli avec succès');
+    $this->info('assistances.company_id rempli avec succès');
     }
 }
